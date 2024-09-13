6c2da962ccf19676e994a39dffe4b0a73d561798
adamwuyu <adam.wuyu@gmail.com>
2024-09-13 09:54:22 +0800

    没想到手机版的应用列表在ChatHistorySlider中，再次修改，从而用普通<a>连接替代router，因为学艺不精，不知道怎么重置表单，导致当前app会按前一个应用 的必选输入项进行检查，而导致错误，暂时改为传统链接

--- projects/app/src/pageComponents/chat/SliderApps.tsx
+++ projects/app/src/pageComponents/chat/SliderApps.tsx
@@ -164,7 +164,7 @@ const SliderApps = ({ apps, activeAppId }: { apps: AppListItemType[]; activeAppI
           >
             <Avatar src={item.avatar} w={'1.5rem'} borderRadius={'md'} />
             <Box ml={2} className={'textEllipsis'} style={{ color: '#101010' }}>
-              {item.name}-{item.type}
+              {item.name}
             </Box>
           </Flex>
         ))}
